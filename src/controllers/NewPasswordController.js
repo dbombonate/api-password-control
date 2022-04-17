7c26bb8ec828fe7b2a7e34f9e20d92575a440476
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-04-17 17:18:02 -0300

    remove variable data

--- src/controllers/NewPasswordController.js
+++ src/controllers/NewPasswordController.js
@@ -2,11 +2,9 @@ import CreatePasswordService from '../services/CreatePasswordService';
 
 class CreatePasswordController {
   async handle(req, res) {
-    const data = req.body;
-
     const createPasswordService = new CreatePasswordService();
 
-    const password = await createPasswordService.create(data);
+    const password = await createPasswordService.create(req.body);
 
     return res.send(password);
   }
